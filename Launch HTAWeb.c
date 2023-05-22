d0574245f7c475958c70e979600c0ed33bc90b16
Mace Cail <116770955+macecailiscool@users.noreply.github.com>
2023-05-22 09:05:02 -0500

    Create Launch HTAWeb.c

---
+++ Launch HTAWeb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    const char* verbose_info[] = {
+        "Setting up Internet Explorer runtime...",
+        "Loading up HTA code...",
+        "Exiting Python loader script..."
+    };
+
+    int verbose_info_length = sizeof(verbose_info) / sizeof(verbose_info[0]);
+
+    for (int i = 0; i < verbose_info_length; i++) {
+        printf("%s\n", verbose_info[i]);
+    }
+
+    const char* hta_file = "htaweb.hta";
+
+    printf("Launching HTA file...\n");
+
+    if (system(hta_file) != 0) {
+        printf("Error launching HTA file.\n");
+    }
+
+    return 0;
+}
