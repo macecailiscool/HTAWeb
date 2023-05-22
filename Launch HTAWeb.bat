bd068954f39d1de474ed58c29480a6b97f468848
Mace Cail <116770955+macecailiscool@users.noreply.github.com>
2023-05-22 08:59:46 -0500

    Create Launch HTAWeb.bat

---
+++ Launch HTAWeb.bat
@@ -0,0 +1,21 @@
+@echo off
+
+setlocal EnableDelayedExpansion
+
+set "verbose_info[0]=Setting up Internet Explorer runtime..."
+set "verbose_info[1]=Loading up HTA code..."
+set "verbose_info[2]=Exiting Python loader script..."
+
+for %%i in (0 1 2) do (
+    echo !verbose_info[%%i]!
+)
+
+set "hta_file=htaweb.hta"
+
+echo Launching HTA file...
+
+start "" "%hta_file%"
+
+if errorlevel 1 (
+    echo Error launching HTA file.
+)
